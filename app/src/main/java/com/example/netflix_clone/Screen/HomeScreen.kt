ce36eadb9f4defbb7e3f22800cdc435927cb0763
Piyush-1707 <spiyush1056@gmail.com>
2024-08-02 19:28:21 +0530

    .

--- app/src/main/java/com/example/netflix_clone/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/netflix_clone/Screen/HomeScreen.kt
@@ -1,7 +1,41 @@
+package com.example.netflix_clone.ui.Screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.netflix_clone.R
+
 @Composable
-fun NetflixScreen() {
+fun HomeScreen() {
     Scaffold(
-        modifier = Modifier.background(color=Color.Black),
+        modifier = Modifier.background(color= Color.Black),
         bottomBar = {
             BottomAppBar(
                 containerColor = Color.Black, modifier = Modifier
