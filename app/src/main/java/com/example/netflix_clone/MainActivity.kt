b27aa9d42c55dd5fe8934a26fbb934c7a94ea59f
Piyush-1707 <spiyush1056@gmail.com>
2024-08-31 12:11:48 +0530

    .

--- app/src/main/java/com/example/netflix_clone/MainActivity.kt
+++ app/src/main/java/com/example/netflix_clone/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
